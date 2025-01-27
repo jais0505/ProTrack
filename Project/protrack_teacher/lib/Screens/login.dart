d30ff8bbcceb07066e74666349f4b4ce94f2ddf1
jais0505 <jaisjose44@gmail.com>
2025-01-27 15:03:15 +0530

    Code updated on 26th 3:05

--- Project/protrack_teacher/lib/Screens/login.dart
+++ Project/protrack_teacher/lib/Screens/login.dart
@@ -1,5 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:protrack_teacher/Screens/dashboard.dart';
+import 'package:cherry_toast/resources/arrays.dart';
+import 'package:cherry_toast/cherry_toast.dart';
+import 'package:protrack_teacher/main.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,6 +14,71 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   bool passkey = true;
+  final TextEditingController _emailEditingController = TextEditingController();
+  final TextEditingController _passwordEditingController =
+      TextEditingController();
+
+  Future<void> signUp() async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(
+              email: _emailEditingController.text,
+              password: _passwordEditingController.text);
+      String uid = userCredential.user!.uid;
+      final response = await supabase
+          .from('tbl_teacher')
+          .select()
+          .eq("teacher_auth", uid)
+          .limit(1);
+      if (response.isNotEmpty) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const DashboardScreen(),
+          ),
+        );
+      } else {
+        print("Invalid email or password");
+        CherryToast.error(
+                description: Text("No teacher found.",
+                    style: TextStyle(color: Colors.black)),
+                animationType: AnimationType.fromRight,
+                animationDuration: Duration(milliseconds: 1000),
+                autoDismiss: true)
+            .show(context);
+      }
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        CherryToast.error(
+                description: Text("No user found for that email.",
+                    style: TextStyle(color: Colors.black)),
+                animationType: AnimationType.fromRight,
+                animationDuration: Duration(milliseconds: 1000),
+                autoDismiss: true)
+            .show(context);
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        CherryToast.error(
+                description: Text("Wrong password provided for that user.",
+                    style: TextStyle(color: Colors.black)),
+                animationType: AnimationType.fromRight,
+                animationDuration: Duration(milliseconds: 1000),
+                autoDismiss: true)
+            .show(context);
+        print('Wrong password provided for that user.');
+      } else {
+        CherryToast.error(
+                description: Text("Error occur in login.",
+                    style: TextStyle(color: Colors.black)),
+                animationType: AnimationType.fromRight,
+                animationDuration: Duration(milliseconds: 1000),
+                autoDismiss: true)
+            .show(context);
+        print("Error occur in login:$e");
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -75,6 +144,7 @@ class _LoginScreenState extends State<LoginScreen> {
                       child: Padding(
                         padding: const EdgeInsets.only(left: 22, right: 22),
                         child: TextFormField(
+                          controller: _emailEditingController,
                           decoration: InputDecoration(
                               labelText: "Email",
                               labelStyle: TextStyle(fontSize: 12),
@@ -90,6 +160,7 @@ class _LoginScreenState extends State<LoginScreen> {
                       child: Padding(
                         padding: const EdgeInsets.only(left: 22, right: 22),
                         child: TextFormField(
+                          controller: _passwordEditingController,
                           decoration: InputDecoration(
                               labelText: 'Password',
                               labelStyle: TextStyle(fontSize: 12),
@@ -135,12 +206,7 @@ class _LoginScreenState extends State<LoginScreen> {
                               shape: RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(5))),
                           onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const DashboardScreen(),
-                              ),
-                            );
+                            signUp();
                           },
                           child: Text(
                             'Login',
